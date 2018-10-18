312d4c15939294dc7c67e5f1518f01cbb03b49c2
tripor <up201605314@fe.up.pt>
2018-10-18 22:08:07 +0100

    reader

--- data.c
+++ data.c
@@ -1,9 +1,30 @@
 #include "data.h"
 
 void changestate2Read(unsigned int message,unsigned int bcc);
+void changestate2Write(unsigned int message);
+void sendDataAux(unsigned int *data,int length,int fd);
 
 int s=0;
 int state2=0;
+
+int count2=1;
+int program_fd2=0;
+unsigned int *data;
+int length;
+void touch2()
+{
+	printf("alarme # %d\n", count2);
+  count2++;
+  if(count2>=4)
+  {
+    printf("Couldn't make connection. Exiting...\n");
+    llClose(program_fd2);
+    exit(1);
+  }
+
+  sendDataAux(data,length,program_fd2);
+  alarm(3);
+}
 
 void writeStuff(unsigned int data,int fd)
 {
@@ -49,7 +70,7 @@ unsigned int unStuff(unsigned int first,unsigned int second)
     return 0x00;
 }
 
-void sendData(unsigned int *data,int length,int fd)
+void sendDataAux(unsigned int *data,int length,int fd)
 {
   unsigned int bcc=0;
   char sendMessage[255]="";
@@ -62,10 +83,19 @@ void sendData(unsigned int *data,int length,int fd)
   {
     bcc^=data[i];
   }
+
   sprintf(sendMessage,"%x",FLAG);
   llWrite(sendMessage,fd);
   strcpy(sendMessage,"");
   sprintf(sendMessage,"%x",Aemi);
+  llWrite(sendMessage,fd);
+  strcpy(sendMessage,"");
+  if(s==0){
+    sprintf(sendMessage,"%x",S0);
+  }
+  else{
+    sprintf(sendMessage,"%x",S1);
+  }
   llWrite(sendMessage,fd);
   strcpy(sendMessage,"");
   sprintf(sendMessage,"%x",bcc);
@@ -82,6 +112,29 @@ void sendData(unsigned int *data,int length,int fd)
   strcpy(sendMessage,"");
   sprintf(sendMessage,"%x",FLAG);
   llWrite(sendMessage,fd);
+}
+
+void sendData(unsigned int *data2,int length2,int fd)
+{
+  signal(SIGALRM, touch2);
+  sendDataAux(data2,length2,fd);
+  program_fd2=fd;
+  data=data2;
+  length=length2;
+  while(state2!=5){
+    char *receive=malloc(255);
+    alarm(3);
+    llRead(&receive,fd);
+    unsigned int flag=0,a=0,c=0,bcc=0,flag2=0;
+    sscanf(receive,"%x %x %x %x %x",&flag,&a,&c,&bcc,&flag2);
+    changestate2Write(flag);
+    changestate2Write(a);
+    changestate2Write(c);
+    changestate2Write(bcc);
+    changestate2Write(flag2);
+  }
+  if(s==0)s=1;
+  else s=0;
 }
 
 
@@ -112,7 +165,7 @@ void readData(int fd)
       i=i-3;
       bcc^=guardar[0];//A
       bcc^=guardar[1];//C
-      for(int j=2;j<=i;j++)
+      for(int j=3;j<=i;j++)
       {
         unsigned int un=unStuff(guardar[j],guardar[j+1]);
         if(j!=i)
@@ -125,16 +178,27 @@ void readData(int fd)
             j++;
           }
         }
+        else
+        {
+          bcc^=guardar[j];
+        }
       }
       changestate2Read(guardar[0],bcc);
       changestate2Read(guardar[1],bcc);
       changestate2Read(guardar[2],bcc);
-      changestate2Read(guardar[i-2],bcc);
-      changestate2Read(guardar[i-1],bcc);
+      changestate2Read(guardar[i+1],bcc);
+      changestate2Read(guardar[i+2],bcc);
 
     }
-    printf("estado:%d\n",state2);
   }
+
+
+  char sendMessage[255]="";
+  if(s==0)
+    sprintf(sendMessage,"%x %x %x %x %x",FLAG,Arec,Crr0,Arec^Crr0,FLAG);
+  else
+    sprintf(sendMessage,"%x %x %x %x %x",FLAG,Arec,Crr1,Arec^Crr1,FLAG);
+  llWrite(sendMessage,fd);
 }
 
 void changestate2Read(unsigned int message,unsigned int bcc)
@@ -192,4 +256,52 @@ void changestate2Read(unsigned int message,unsigned int bcc)
       break;
       default: state2 = 0;
     }
+}
+void changestate2Write(unsigned int message)
+{
+  switch(state2){
+    case 0:
+    if(message == FLAG)
+        state2 = 1;
+    break;
+
+    case 1:
+    if(message == Aemi)
+        state2 = 2;
+    else if(message != FLAG)
+        state2 = 0;
+    break;
+
+    case 2:
+    if(message == S0 && s==0)
+    {
+      s=1;
+      state2 = 3;
+    }
+    else if(message == S1 && s==1)
+    {
+      s=0;
+      state2 = 3;
+    }
+    else if (message == FLAG)
+        state2 = 1;
+    else
+        state2 = 0;
+    break;
+    case 3:
+    if((message == (Arec^Crr0) && s==1) || (message == (Arec^Crr1) && s==0) )
+        state2 = 4;
+    else if (message == FLAG)
+        state2 = 1;
+    else
+        state2 = 0;
+    break;
+    case 4:
+    if(message == FLAG)
+    state2 = 5;
+    else state2 = 0;
+
+    break;
+    default: state2 = 0;
+  }
 }
--- macros.h
+++ macros.h
@@ -6,7 +6,7 @@
 #define Cset 0x03
 #define Cdisc 0x11
 #define Cua 0x08
-#define crr1 0x85
+#define Crr1 0x85
 #define Crr0 0x05
 #define Crej1 0x81
 #define Crej0 0x01
