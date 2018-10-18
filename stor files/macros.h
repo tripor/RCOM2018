57a06c7b563a33ffe811e5e723aafe0f3912c1e4
tripor <up201605314@fe.up.pt>
2018-10-18 15:36:34 +0100

    macros.h

---
+++ stor files/macros.h
@@ -0,0 +1,15 @@
+#ifndef MACROS_H_
+
+#define FLAG 0x7E
+#define Aemi 0x03
+#define Arec 0x01
+#define Cset 0x03
+#define Cdisc 0x11
+#define Cua 0x08
+#define crr1 0x85
+#define Crr0 0x05
+#define Crej1 0x81
+#define Crej0 0x01
+
+
+#endif
