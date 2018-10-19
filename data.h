f484d08a84d881f2aee4fedb8ab662cf89a1649d
Unknown <joaoalvaro.ferreira@hotmail.com>
2018-10-19 12:27:41 +0100

    ahhhhhhhhhhhhhhhhh

--- data.h
+++ data.h
@@ -4,3 +4,7 @@
 
 void sendData(unsigned int *data,int length,int fd);
 void readData(int fd,unsigned int *guardar);
+void writeStuff(unsigned int data,int fd);
+void touch2();
+unsigned int unStuff(unsigned int first,unsigned int second);
+void sendDataAux(unsigned int *data,int length,int fd);
--- testApplication.c
+++ testApplication.c
@@ -21,9 +21,8 @@ void applicationSend(char fd, char* path){
 
     sendControlPackage(fd, size, filename, 0); //start
 
-    //fazer loop de mandar packages com o sendDataPackage, quero ter a certeza do tamanho a usar/como fazer divisões
-
-    sendControlPackage(fd, size, filename, 0);//end
+    //FAZER O WHILE E O SPLIT MESSAGE
+    sendControlPackage(fd, size, filename, 1);//end
 
     return;
 }
@@ -101,18 +100,109 @@ void sendControlPackage(int fd, int size, char* filename, int startOrEnd){
 
 }
 
-void receiveData(char* path){
+void receiveData(char* path, int fd){
 
+  STOP=FALSE;
   unsigned int* fullMessage[100];
   unsigned int thisByte;
-  void readData(int fd,unsigned int *fullMessage);
 
-  if(fd[0] != StartC)
-    {
-      printf("ERROR: Doesn't have start control package;")
+  //Control Package
+  unsigned int CData;
+  unsigned int type;
+  unsigned int length;
+  unsigned int lengthCounter = 0;
+  unsigned int fileSize;
+  char fileName;
+  unsigned int ctrlPackageSize;
+
+  //Data Package
+  unsigned int Cbyte;
+  unsigned int Nread[2];
+  unsigned int l2;
+  unsigned int l1;
+  unsigned int k; // l2 * 256 + l1;
+  unsigned int dataCounter = 0;
+
+
+  readData(int fd,unsigned int *fullMessage);
+
+
+//Leitura control package START
+  CData = fullMessage[0];
+  if(CData != StartC)
+    printf("ERROR: Received data is wrong, doesn't start with the correct control package, current value: %x", CData);
+
+  type = fullMessage[1]; //Ver FileSize
+  if(type != 0)
+    printf("ERROR: Received data is wrong, doesn't have the correct type value for the information sent (0), current value: %x", type);
+
+  length = fullMessage[2];
+  unsigned int* value[length];
+
+  while(lengthCounter < length)
+  {
+    value[lengthCounter] = fullMessage[2 + lengthCounter]
+    lengthCounter++;
+  }
+  fileSize = value;
+
+
+
+  type = fullMessage[3 + lengthCounter]; //Ver fileName
+  if(type != 1)
+    printf("ERROR: Received data is wrong, doesn't have the correct type value for the information sent (1), current value: %x", type);
+
+  length = fullMessage[4 + lengthCounter];
+  unsigned int* value[length];
+  lengthCounter = 0;
+  while(lengthCounter < length)
+  {
+    value[lengthCounter] = fullMessage[2 + lengthCounter]
+    lengthCounter++;
+  }
+
+  fileName = value;
+
+  ctrlPackageSize = length*2 + 5;
+
+  //Leitura Data Package
+  Cbyte = fullMessage[ctrlPackageSize + 1]
+  if(CData != Cbyte)
+    printf("ERROR: Received data is wrong, doesn't start with the data package, current value %x", Cbyte);
+
+  Nread[0] = fullMessage[ctrlPackageSize + 2];
+  Nread[1] = fullMessage[ctrlPackageSize + 3];
+  if(Nread != 255)
+    printf("ERROR: Received data is wrong,parsed N value is not 255, current value %x", Nread);
+
+  l2 = fullMessage[ctrlPackageSize + 4];
+  l1 = fullMessage[ctrlPackageSize + 5];
+  k = l2 * 256 + l1;
+  unsigned int data_content[k];
+
+  while(dataCounter < k)){
+    data_content[k] = fullMessage[ctrlPackageSize + 5 + dataCounter];
+    dataCounter++;
+  }
+
+  //Leitura control package END
+  unsigned int tmpPackage1[ctrlPackageSize], tmpPackage2[ctrlPackageSize];
+
+  tmpPackage1 = fullMessage >> (ctrlPackageSize + 5 + dataCounter);
+  tmpPackage2 = fullMessage%(ctrlPackageSize + 5 + dataCounter);
+
+  tmpPackage1 = tmpPackage1 >> 1;
+
+  if(tmpPackage2[0] == EndC)
+    printf("ERROR: Received data is wrong, doesn't start with the correct control package, current value: %x", tmpPackage2[0]);
+
+    tmpPackage2 = tmpPackage2 >> 1;
+
+    if(tmpPackage2 != tmpPackage1){
+    printf("ERROR: Control packages don't matcH");
     }
 
-  //resto faço amanhã
+    return;
 
 
 }
